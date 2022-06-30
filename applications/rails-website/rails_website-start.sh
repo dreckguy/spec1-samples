99642615d3bbb04d8dbaca8146ed55c262136c9c
ophir.h <ophir.h@quali.com>
2022-06-30 19:14:36 +0300

    remove all but 1 blueprints

--- applications/rails-website/rails_website-start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "****************************************************************"
-echo "Starting Rails Server"
-echo "****************************************************************"
-
-cd /home/ubuntu/wwwroot || exit
-rails server --binding=localhost
