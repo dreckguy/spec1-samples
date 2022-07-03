9043654b64b1c128c8ed146420a521d8e0f0de7b
ophir.h <ophir.h@quali.com>
2022-07-03 17:12:13 +0300

    add debugging

--- stack.tf
+++ stack.tf
@@ -1,5 +1,6 @@
-resource "aws_cloudformation_stack" "network" {
+resource "aws_cloudformation_stack" "stack" {
     name = "spec1-in-spec2"
-    template_url = "https://github.com/dreckguy/spec1-samples/blob/main/template.json"
+    template_url = "https://cf-templates-653mmcjy71ia-eu-west-1.s3.eu-west-1.amazonaws.com/template.json"
+    capabilities = ["CAPABILITY_AUTO_EXPAND","CAPABILITY_NAMED_IAM"]
 
 }
