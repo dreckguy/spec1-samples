b2eec40ed010753eeacb9e35494296f2a2ca4203
ophir.h <ophir.h@quali.com>
2022-07-04 15:23:03 +0300

    remove userdata

--- stack.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_cloudformation_stack" "stack" {
-    name = "spec1-in-spec2"
-    template_url = "https://cf-templates-653mmcjy71ia-eu-west-1.s3.eu-west-1.amazonaws.com/template.json"
-    capabilities = ["CAPABILITY_AUTO_EXPAND","CAPABILITY_NAMED_IAM"]
-
-}
